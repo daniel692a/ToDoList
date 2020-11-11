03ea85400fbd2387d79296f1590356b95898553a
Daniel Armas <daniel62armas@gmail.com>
2020-11-11 13:30:32 -0600

    Connect API-

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://stoic-mcnulty-502544.netlify.app',
   cache: new InMemoryCache()
 })
 
